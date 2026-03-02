05a02b8d6a9f9f9a4e848307bd2d899954995381
abbasawad25 <abbasawad916@gmail.com>
2026-03-02 10:43:20 -0800

    added file edit and add lanage

---
+++ tools/languages/ar.php
@@ -0,0 +1,22 @@
+<?php
+// ar Language for rix PHP Script
+// Last update: 02/03/2026 14:53
+$lang = array();
+$lang["dir"] = "rtl";
+$lang["lang"] = "ar";
+$lang["site_name"] = "ريكس سوفت";
+$lang["site_title"] = "خدمة إنشاء مواقع";
+$lang["site_description"] = "ويلزواف - مرحبًا بكم في مدونتي التي تنشر دورات تعليمية حول البرمجة والقرصنة والقرصنة والشبكات ولغات البرمجة والنصوص المجانية والمدفوعة والبرامج النصية والقوالب.";
+$lang["site_keywords"] = "ويب سايت,  مواقع ويب ,خدمة انشاء مواقع";
+$lang["home"] = "home";
+$lang["created_web_site"] = "الدولة التى يوجد بها الموقع";
+$lang["div_description"] = "احصل علي افضل التصاميم في عالم الويب سايت ";
+$lang["subscribe"] = " اشتراك";
+$lang["subscribe_to_newsletter"] = "اشتراك في خدمة الاخبار";
+$lang["email"] = "البريد الالكتروني ";
+$lang["description_css"] = "تستخدم في تنسيق وجعل الموقع اكثر حاذبية";
+$lang["description_html"] = "تستخدم في بناء هيمل الموقع";
+$lang["graphic_design"] = "جرافيك ";
+$lang["description_graphic_design"] = "احصل علي افضل التصاميم في مجال الجرافيك";
+$lang["description_copyright"] = "جميع الحقوق المحفوظة بواسطة ريكس سوفت";
+?>
